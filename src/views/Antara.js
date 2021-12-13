63a45787eb006620abcf57d9fe242c7d8f3fa155
rango527 <web.mobile497373@gmail.com>
2021-12-14 02:33:24 +0800

    fix antara css

--- src/views/Antara.js
+++ src/views/Antara.js
@@ -4,8 +4,6 @@ import React from "react";
 const Antara = () => {
     return (
         <div id="section-2" className="section---light movie section-2 wf-section">
-            <h1 id="Antara" className="style-2---h1 antara">MOVIE&nbsp;NFT VIDEO</h1>
-            <h1 className="style-2---h1 antara mobile">MOVIE&nbsp;NFT VIDEO</h1>
             <div className="w-layout-grid grid-4">
                 <div>
                     <a href="https://discord.gg/bcY9thmKy4" target="_blank" className="button-3 mobile w-button" rel="noreferrer"></a>
@@ -20,6 +18,8 @@ const Antara = () => {
                     <a href="https://twitter.com/ArabianCamels" target="_blank" className="button-6 mobile w-button" rel="noreferrer"></a>
                 </div>
             </div>
+            <h1 id="Antara" className="style-2---h1 antara">MOVIE&nbsp;NFT VIDEO</h1>
+            <h1 className="style-2---h1 antara mobile">MOVIE&nbsp;NFT VIDEO</h1>
             <div className="container yt antara-video">
                 <div style={{paddingTop:"56.17021276595745%"}} className="w-embed-youtubevideo youtube-2"><iframe src="https://www.youtube.com/embed/ey1bYR88hF4?rel=0&amp;controls=1&amp;autoplay=1&amp;mute=0&amp;start=0" frameBorder="0" style={{position:"absolute", left:"0", top:"0", width:"100%", height:"100%", pointerEvents:"auto"}} allow="autoplay; encrypted-media" allowFullScreen="" title="Movie-video"></iframe></div>
             </div>
