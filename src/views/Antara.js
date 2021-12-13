bb52203d167eaad2fb8817af1c68dc8e79313db7
rango527 <web.mobile497373@gmail.com>
2021-12-14 01:21:09 +0800

    fix antara css

--- src/views/Antara.js
+++ src/views/Antara.js
@@ -20,7 +20,7 @@ const Antara = () => {
                     <a href="https://twitter.com/ArabianCamels" target="_blank" className="button-6 mobile w-button" rel="noreferrer"></a>
                 </div>
             </div>
-            <div className="container yt">
+            <div className="container yt antara-video">
                 <div style={{paddingTop:"56.17021276595745%"}} className="w-embed-youtubevideo youtube-2"><iframe src="https://www.youtube.com/embed/ey1bYR88hF4?rel=0&amp;controls=1&amp;autoplay=1&amp;mute=0&amp;start=0" frameBorder="0" style={{position:"absolute", left:"0", top:"0", width:"100%", height:"100%", pointerEvents:"auto"}} allow="autoplay; encrypted-media" allowFullScreen="" title="Movie-video"></iframe></div>
             </div>
             <img src="images/arabian_camel_wen-Lembo.png" loading="lazy" sizes="100vw" srcSet="images/arabian_camel_wen-Lembo-p-500.png 500w, images/arabian_camel_wen-Lembo-p-800.png 800w, images/arabian_camel_wen-Lembo-p-1080.png 1080w, images/arabian_camel_wen-Lembo-p-1600.png 1600w, images/arabian_camel_wen-Lembo-p-2000.png 2000w, images/arabian_camel_wen-Lembo-p-2600.png 2600w, images/arabian_camel_wen-Lembo.png 3000w" alt="" className="image-7 cam2" />
