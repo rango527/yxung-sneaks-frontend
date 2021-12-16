8faf71e6c69a74c5cda250c55fcbf92daac08c5b
rango527 <web.mobile497373@gmail.com>
2021-12-16 15:10:35 +0800

    fix home

--- src/views/Home.js
+++ src/views/Home.js
@@ -5,14 +5,15 @@ import slider1 from '../images/slider-img.png';
 const Home = ({ isPresale = true }) => {
     return (
         <section id="Home" className="section---light demo-4---home-hero wf-section">
-            {!isPresale && <a href="https://mgu9490eyrw.typeform.com/to/ouxOMiDB" target="_blank" className="button movie w-button" rel="noopener noreferrer">
-                WhiteList
-            </a>}
             <div className='section---light demo-4---home-hero home-section'>
                 <div className="container header-text">
-                    <h1 className="style-2---h1">Welcome to<br></br>The Caravan</h1>
-                    <p className="paragraph-4">Arabian Camels is the first NFT community to create a Hollywood movie together. Linking movies and gaming with NFTs and DeFi. The first movie in history funded by an NFT - Owned by everyone.</p>
-                    <p className="paragraph-4 text">The Antara Movie NFT<br></br>-COMING&nbsp;SOON!-<br></br>#TogetherWeRide</p>.
+                    <h1 className="style-2---h1 h1-welcome">Welcome to<br></br>the caravan!</h1>
+                    <p className="paragraph-4" style={{marginBottom: "0", fontSize: "22px"}}>Arabian Camels is the first NFT community to create a Hollywood movie together. Linking movies and gaming with NFTs and DeFi. The first movie in history funded by an NFT</p>
+                    <p className="paragraph-4 text" style={{margin: "0", padding: "0", fontSize: "28px"}}>Owned by everyone.</p>
+                    <p className="paragraph-4 text" style={{textAlign: "center", fontSize: "28px"}}>-COMING&nbsp;SOON!-<br></br>The Antara Movie NFT<br></br>#TogetherWeRide</p>
+                    {!isPresale && <a href="https://mgu9490eyrw.typeform.com/to/ouxOMiDB" target="_blank" className="button movie w-button" rel="noopener noreferrer" style={{marginTop: "25px", marginBottom: "25px"}}>
+                        WhiteList
+                    </a>}
                 </div>
                 <img className="bg-image" src={slider1} alt=""/>
             </div>
