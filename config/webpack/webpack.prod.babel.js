d205ffb4883fdb59c2e5738e7f4316eb523207f2
rango527 <web.mobile497373@gmail.com>
2022-04-14 14:33:11 +0800

    fix packages

--- config/webpack/webpack.prod.babel.js
+++ config/webpack/webpack.prod.babel.js
@@ -9,6 +9,16 @@ module.exports = {
         path: paths.outputPath,
         chunkFilename: '[name].[chunkhash].js'
     },
+    performance: {
+        hints: 'warning',
+        maxAssetSize: 20000000,
+        maxEntrypointSize: 8500000,
+        assetFilter: assetFilename => {
+            return (
+                assetFilename.endsWith('.css') || assetFilename.endsWith('.js')
+            );
+        }
+    },
     plugins: [
         new CleanWebpackPlugin()
     ],
